e6b91e6e6f3f3d3d6b51258fa909a85564c90fd7
YSM-WEB <ysmin996@gmail.com>
2021-01-02 21:25:21 +0900

    foot update

--- clone/shopping/script/slide.js
+++ clone/shopping/script/slide.js
@@ -24,37 +24,36 @@ window.onload=function(){
     var wid=productWrap.clientWidth/3;
     for(var i=0; i<productSlide.length; i++){
         productSlide[i].style.width=wid;
-    }   
+    }
    }else{
        var wid=productWrap.clientWidth/4;
     for(var i=0; i<productSlide.length; i++){
         productSlide[i].style.width=wid;
     }
-
    }
 };
 
 window.addEventListener('resize',function(){
- slide();   
+ slide();
 });
-        
-function move(){      
+
+function move(){
     slideul.style.transition = '0.2s';
     slideul.style.transform = "translate3d("+leftminus*(curIndex+1)+"px, 0px, 0px)";
     if(curIndex==3){
         cnt[curIndex].style.backgroundColor="white";
-        cnt[0].style.backgroundColor="black";    
+        cnt[0].style.backgroundColor="black";
     }else{
     cnt[curIndex].style.backgroundColor="white";
     cnt[curIndex+1].style.backgroundColor="black";
     }
     curIndex++;
- 
+
     if(curIndex === 4){
         setTimeout(function(){
             slideul.style.transition = '0s';
             slideul.style.transform = "translate3d(0px, 0px, 0px)";
-            
+
         },201);
         curIndex = 0;
         }
@@ -63,10 +62,10 @@ function move(){
 function activeCnt(e){
     e.preventDefault();
     curIndex=e.target.innerHTML-1;
-    
+
     for(var i=0; i<cnt.length; i++){
         if(cnt[i]==e.target){
-            e.target.style.backgroundColor="black";        
+            e.target.style.backgroundColor="black";
         }else cnt[i].style.backgroundColor="white";
     }
     slideul.style.transition = '0.2s';
